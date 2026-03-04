1d1fbc855f02617555fc5fa04bb315c9366376a3
lvzhenbo <294221990@qq.com>
2026-03-05 02:45:11 +0800

    重命名设置页面组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,7 +43,7 @@ const router = createRouter({
         {
           path: 'setting',
           name: 'Setting',
-          component: () => import('@/views/Setting/Setting.vue'),
+          component: () => import('@/views/Setting/SettingView.vue'),
         },
       ],
     },
